ae0ccb2ff9bdf2de880c886b7066d450aa84d8b3
Ahmedglez <ahmediglez@gmail.com>
2022-06-30 19:00:17 -0400

    change #8

--- utils/update_system.py
+++ utils/update_system.py
@@ -38,21 +38,7 @@ def confirm1():
                         break
                     else:
                         print('Opcion no valida. Intentelo de nuevo')
-            except subprocess.TimeoutExpired as exc:
-                subprocess.Popen.wait();
-                print("La ejecucion de este comando está demorado demasiado tiempo")
-                print("Desea continuar con la ejecucion?")
-                print('S-Si    -   N-No')
-                answer = 0
-                while(answer != 'S' and answer != 'N'):
-                    answer = input().capitalize()
-                    if(answer == 'S'):
-                        print("Actualizando paquetes del sistema... ")
-                        
-                    elif(answer == 'N'):
-                        subprocess.Popen.terminate();
-                    else:
-                        print('Opcion no valida')
+            
 
         elif(answer == 'N'):
             answer2 = 0
